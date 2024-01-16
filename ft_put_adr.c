6870ee56c8d4fbf00d70874dbe2a8ee6b9cd7526
reduno96 <reduno96@gmail.com>
2024-01-16 22:07:22 +0100

    Add ft_put_adr function

---
+++ ft_put_adr.c
@@ -0,0 +1,13 @@
+#include "ft_printf.h"
+
+int ft_put_adr(void *adr)
+{
+    int count;
+
+    count = 0;
+    if (adr == NULL)
+        return (count += ft_put_str("0x0"));
+    count += ft_put_str("0x");
+    count += ft_put_hexa((unsigned long long)adr);
+    return (count);
+}
